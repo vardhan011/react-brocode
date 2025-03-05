7fd5a047f9edd28c90c585946e363feb10413709
Govardhan reddy Borra <borragovardhanreddy@gmail.com>
2025-03-05 14:08:20 +0530

    initial commit travel journal

---
+++ travel/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+       <header className="headermain">
+        <img className="headerlogo" src="src/assets/react.svg" alt="logo"/>
+        <h1 className="headertext">Travel Journal</h1>
+       </header>
+    );
+}
